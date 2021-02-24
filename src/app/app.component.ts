2ba1304c74da89fb18696678be5bc2d8907cb004
Suraj.Mandal <Suraj.Mandal@ibm.com>
2021-02-25 01:17:47 +0530

    modifications made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CovidService} from './services/covid.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'covid19Application';
+  worldDetails: {};
+
+  constructor(private covidService: CovidService) {
+  }
+
+  ngOnInit(): void {
+    this.covidService.getWorldTotalDetails().subscribe(data => {
+      console.log(data);
+      this.worldDetails = data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {HeaderComponent} from './header/header.component';
+import {SearchBarComponent} from './search-bar/search-bar.component';
+import {FormsModule} from '@angular/forms';
+import {CovidService} from './services/covid.service';
+import {RouterService} from './services/router.service';
+import {RouterModule, Routes} from '@angular/router';
+import {CountryDetailComponent} from './country-detail/country-detail.component';
+import {ChartsModule} from 'ng2-charts';
+
+const routes: Routes = [{
+  component: HeaderComponent,
+  path: ''
+}, {
+  component: CountryDetailComponent,
+  path: 'country'
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    CountryDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ChartsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [CovidService, RouterService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
